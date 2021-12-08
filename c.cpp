b79c86b8c2754b78b4c64b0362bf1f330f9fdc9d
luana200000 <95723250+luana200000@users.noreply.github.com>
2021-12-08 20:04:59 -0300

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,45 @@
+//luana Teixeira de Moraes
+
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define l 10
+#define p 15
+
+int main(void){
+	
+	setlocale(LC_ALL,"portuguese");
+	
+	int logic[l];
+	
+	int program[p];
+	
+	int i, j;
+	
+	printf("\nDigite o número de matricula: ");
+	
+	for(i=0; i<l; i++){
+		
+		scanf("%i",&logic[i]);
+	}
+	printf("\nDigite o número de matricula: ");
+	
+	for(j=0; j<p; j++){
+		
+		scanf("%i",&program[j]);
+	}
+
+	for(i = 0; i<l; i++){
+		
+		if(i < l){
+			
+			printf("\n [%d]\t",logic[i]);
+			
+			printf("\t\t [%d]",program[i]);
+		}
+		if(logic[i]==program[i]){
+			
+			printf("\t\t   [%i]\t",logic[i]);
+		}
+	}
+}
